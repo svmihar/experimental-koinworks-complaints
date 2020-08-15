9214ffb4c4019d5bef54a7c4a544eb1432071acc
svmihar <svmihar@gmail.com>
2020-08-15 23:51:59 +0700

    :zap: new vectorizer, karena butuh csr sparse matrix buat compute cosine distance (gak ribet convert sana sini)

--- eda.py
+++ eda.py
@@ -1,4 +1,6 @@
-from texthero.representation import tfidf, pca, kmeans
+from texthero.representation import pca, kmeans
+from sklearn.feature_extraction.text import TfidfVectorizer
+import joblib
 import pandas as pd
 
 df = pd.read_csv("koinworks_cleaned.csv")
@@ -8,6 +10,11 @@ print(f"before drop duplicate: {len(df)}")
 df = df.drop_duplicates(subset=["cleaned"])
 print(f"after drop duplicate: {len(df)}")
 df.dropna(inplace=True)
-df["tfidf"] = df["cleaned"].pipe(tfidf)
-df["pca"] = df["tfidf"].pipe(pca)
+
+vectorizer  = TfidfVectorizer()
+X = vectorizer.fit_transform(df.cleaned.values)
+df["tfidf_dense"] = [list(a) for a in X.toarray()]
+df["pca"] = df["tfidf_dense"].pipe(pca)
 df.to_parquet("koinworks_fix.pkl", index=False)
+df['tfidf']=X
+joblib.dump(X,'tfidf.pkl')
